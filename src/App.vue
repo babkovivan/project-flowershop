66825c8745aded9894f8630eaf416a7b7e0bb3be
babkovivan <babkov1738@gmail.com>
2025-03-06 22:37:40 +0300

    123

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-gray-100 min-h-screen font-sans">
+      <Header />
+      <HeroSection />
+      <PopularProducts />
+      <Catalog />
+      <MapSection />
+    </div>
+  </template>
+  
+  <script setup>
+  import Header from './components/Header.vue';
+  import HeroSection from './components/HeroSection.vue';
+  import PopularProducts from './components/PopularProducts.vue';
+  import Catalog from './components/Catalog.vue';
+  import MapSection from './components/MapSection.vue';
+  </script>
+  
+  <style scoped></style>
+  
---
+++ src/components/MapSection.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="p-8\">
+      <iframe
+        class="w-full h-64 rounded-xl shadow-lg\"
+        src="https://yandex.ru/map-widget/v1/?ll=30.315877%2C59.939099&z=10\"
+      ></iframe>
+    </section>
+  </template>
+  
+  <script setup></script>
